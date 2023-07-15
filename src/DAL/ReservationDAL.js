ae9667eca93ec3b3a68a511b8ac24f2ba74a428f
btbs <aowl10012004@gmail.com>
2023-07-16 00:34:37 +0800

    Update ReservationDAL.js

--- src/DAL/ReservationDAL.js
+++ src/DAL/ReservationDAL.js
@@ -1,29 +1,27 @@
+export const addReservation = async (req) => {
+  const date = new Date();
+  console.log(date);
+  const response = await fetch(
+    "https://3w1dvat749.execute-api.ap-southeast-2.amazonaws.com/ReservationsDAL",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        TableName: "Reservation",
+        Item: {
+          Email: "johnnytest@gmail.com",
+          DateTime: "test:testam",
+        },
+      }),
+    }
+  );
+  const body = await response.json();
+};
 
-
-export const addReservation = async (req) =>{
-    const date = new Date();
-    const response = await fetch(
-      "https://3w1dvat749.execute-api.ap-southeast-2.amazonaws.com/ReservationsDAL",
-      {
-        method:"POST",
-        body:JSON.stringify({
-            TableName:"Reservation",
-            Item:{
-                    Email:"johnnytest@gmail.com",
-                    DateTime: "test:testam"
-                }
-        })
-      }
-    );
-    const body = await response.json();
-    console.log(response,body)
-}
-
-export const getReservations = async() =>{
-    const response = await fetch(
-        "https://3w1dvat749.execute-api.ap-southeast-2.amazonaws.com/ReservationsDAL?TableName=Reservation",
-    );
-    const body = await response.json();
-    const data = body.Items;
-    return data;
-}
+export const getReservations = async () => {
+  const response = await fetch(
+    "https://3w1dvat749.execute-api.ap-southeast-2.amazonaws.com/ReservationsDAL?TableName=Reservation"
+  );
+  const body = await response.json();
+  const data = body.Items;
+  return data;
+};
